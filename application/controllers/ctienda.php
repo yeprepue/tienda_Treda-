77da95938cc18ad740a5df3e25d05a042712f9e6
attiq <yepretel@misena.edu.co>
2020-11-22 16:20:08 -0500

    Tienda finalizado
    Se realiza creación de dos modilos para la prueba de Treda, se genera carpeta con base de datos y archovo de como se realiza la configuración de la aplicación

--- application/controllers/ctienda.php
+++ application/controllers/ctienda.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ctienda extends CI_Controller {
 
-	
+	    
 	public function index()
 	{
 		$this->load->view('admin/dashboard/list');
